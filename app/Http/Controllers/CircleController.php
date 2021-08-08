08cd078bc8c9a5c7fe2ffe5c1ff2c4469b6dae7d
Troia <vadym.queensoft@gmail.com>
2021-08-08 12:57:20 +0300

    добавил удалить

--- app/Http/Controllers/CircleController.php
+++ app/Http/Controllers/CircleController.php
@@ -41,4 +41,26 @@ class CircleController extends Controller
         return json_encode($result, true);
     }
 
+    public function DeleteTask(request $req){
+        $result = ['status' => false, 'error' => 'Для начала авторизуйтесь'];
+        $UserController = new UserController;
+        $CheckAuth = $UserController->CheckAuth($req);
+        if($CheckAuth['status'] == true){
+            $id = $req['id'];
+            $check_task = task::select()->where(['id' => $id])->first();
+            if($check_task != null){
+                $check_circle = circle::select()->where(['id' => $check_task->circle_id, 'user_id' => $CheckAuth['data']->id])->first();
+                if($check_circle != null){
+                    task::where(['id' => $id])->delete();
+                    $result = ['status' => true];
+                } else {
+                    $result['error'] = "Такого задания не существует";
+                }
+            } else {
+                $result['error'] = "Такого задания не существует";
+            }
+        }
+        return json_encode($result, true);
+    }
+
 }
